a3f78a07a00f8915f8c67688e11e9d05eb76697b
adevlinb <adevlin.booth@gmail.com>
2023-11-25 20:14:50 -0800

    update contact screen

--- src/Screens/ContactScreen.js
+++ src/Screens/ContactScreen.js
@@ -1,5 +1,7 @@
 // IMPORTS
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, Pressable } from 'react-native'
+import { useContext } from 'react';
+import { User } from '../Context/UserContext';
 
 // COMPONENTS
 import Header from '../Components/Header';
@@ -9,62 +11,119 @@ import BottomNav from '../Navigation/BottomNav';
 
 
 export default function ContactScreen({ navigation }) {
+	const { user } = useContext(User);
+
+
 	return (
-        <SafeAreaView style={styles.mainContainer} contentContainerStyle={styles.mainContentContainer}>
+        <SafeAreaView style={styles.mainContainer}>
             <Header navigation={navigation} />
-            <View style={styles.statsContainer}>
-                <View style={styles.stats}>
-                    <Text>Stats</Text>
-                </View>
-                <View style={styles.refresh}>
-                    <Text>Contact Us Screen</Text>
-                </View>
+			<View style={styles.userInfoContainer}>
+                <View style={styles.picContainer}><Image source={{ uri: user.profilePic}} style={styles.profilePic}/></View>
+                <Text style={styles.userName}>{user.name}</Text>
             </View>
+			<View style={styles.mainInputContainer}>
+				<View style={styles.nameInputContainer}>
+					<View style={styles.inputContainer}>
+						<Text style={styles.placeholderText}>First Name</Text>
+						<TextInput></TextInput>
+					</View>
+					<View style={styles.inputContainer}>
+						<Text style={styles.placeholderText}>Last Name</Text>
+						<TextInput></TextInput>
+					</View>	
+				</View>
+				<View style={styles.inputContainer}>
+					<Text style={styles.placeholderText}>Email</Text>
+					<TextInput inputMode="email" placeholder="example@example.com"></TextInput>
+				</View>
+				<View style={styles.inputContainer}>
+					<Text style={styles.placeholderText}>Phone Number</Text>
+					<TextInput inputMode='tel' placeholder="(000) 000-0000"></TextInput>
+				</View>
+				<View style={styles.textBox}>
+					<Text style={styles.placeholderText}>Types your message here...</Text>
+					<TextInput style={styles.textBoxInput} multiline numberOfLines={4}></TextInput>
+				</View>
+			</View>
+			<View style={styles.submitContainer}>
+				<Pressable style={styles.submitButton}><Text style={{color: "white"}}>Submit Button</Text></Pressable>
+			</View>
             <BottomNav navigation={navigation}/>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    mainContentContainer: {
-        // flexGrow: 1,
-    },
     mainContainer: {
         flexGrow: 1
     },
-    mainView: {
-        padding: 5,
-        height: "100%",
+	userInfoContainer: {
         alignItems: "center",
-        paddingTop: 50
+        justifyContent: "center",
+        height: "25%",
     },
-    statsContainer: {
+    picContainer: {
+        backgroundColor: "red",
+        borderRadius: "50%",
+        overflow: "hidden",
+        margin: 10
+    },
+    profilePic: {
+        width: 90,
+        height: 90
+    },
+    userName: {
+        textDecorationLine: 'underline',
+    },
+	mainInputContainer: {
+		width: "100%",
+		height: "40%",
+		justifyContent: "space-evenly",
+		paddingHorizontal: 25,
+		gap: 10
+	},
+	nameInputContainer: {
+		flexDirection: "row",
+	},
+	inputContainer: {
+        borderStyle: "solid",
+        borderWidth: 1,
+		borderColor: "#B0B0B0",
+		width: "100%",
+		height: 40,
+		paddingHorizontal: 5,
+		justifyContent: "center",
+
+	},
+	placeholderText: {
+		color: "#B0B0B0",
+		fontSize: "10px"
+	},
+	textBox: {
+		flexGrow: 1,
+		borderStyle: "solid",
+        borderWidth: 1,
+		borderColor: "#B0B0B0",
+		width: "100%",
+		height: 40,
+		paddingVertical: 5,
+		paddingHorizontal: 5,
+		justifyContent: "center",
+	},
+	textBoxInput: {
+		flexGrow: 1,
+	},
+	submitContainer: {
+		flexGrow: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	submitButton: {
+		padding: 10,
+        borderRadius: 7.5,
+        width: "50%",
         alignItems: "center",
-        justifyContent: "space-between",
-        width: "100%",
-        borderRadius: 5
-    },
-    stats: {
-        flexDirection: "row",
-        backgroundColor: "rgba(0,0,0,0.5)",
-        alignItems: "center",
-        justifyContent: "space-between",
-        width: "100%",
-        padding: 10,
-        zIndex: 5,
-        height: 60,
-    },
-    refresh: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        width: "100%",
-        padding: 10,
-        height: 60,
-        backgroundColor: "#9DBAB9",
-        zIndex: 3,
-        position: "absolute",
-        top: 50
-    },
+        backgroundColor: "rgb(24,46,42)",
+	}
 
 })
